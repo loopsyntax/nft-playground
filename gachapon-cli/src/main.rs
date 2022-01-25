0345ca9dd4833c0f82cf905c7897c6baaa012571
loopsyntax <softechstar@outlook.com>
2022-01-25 23:09:16 +0900

    generate candidate box for nfts

--- gachapon-cli/src/main.rs
+++ gachapon-cli/src/main.rs
@@ -3,26 +3,37 @@ use ergo_node_interface::*;
 use reqwest::blocking::get;
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    println!(" {}", args[1]);
+  let args: Vec<String> = std::env::args().collect();
+  println!("ARGS: {}", args[1]);
 
-    let node = acquire_node_interface_from_local_config();
-    let block_height = node.current_block_height().unwrap();
-    let artist_address = node.wallet_addresses().unwrap()[0].clone();
+  let node = acquire_node_interface_from_local_config();
+  let block_height = node.current_block_height().unwrap();
+  let artist_address = node.wallet_addresses().unwrap()[0].clone();
 
-    // Acquire the ergs_box_for_fee
-  let tx_fee = 10;
+  // Acquire the ergs_box_for_fee
+  let tx_fee = 10000000;
   let ergs_box_for_fee =
-                get_ergs_box_for_fee(artist_address.clone(), tx_fee);
-    GachaponProtocol::mint("Name".to_string(),
-                           "description".to_string(),
-                           0.1, 10, // royalty, start_gacha_height
-                           10, // artisrt fee
-                           block_height, //current_height
-                           artist_address, // artist address
-                           tx_fee, // transaction fee
+        get_ergs_box_for_fee(artist_address.clone(), tx_fee);
+  println!("calling mint");
+    let nft = NFTMetadata{name: "dummy_nft_1".to_string(),
+                          description: "dummy description".to_string(),
+                          asset_type: "text".to_string(),
+                          url: "http://dummy_url".to_string(),
+                          bytes: "dummy".as_bytes().to_vec(),
+                          };
+    println!("Preparing NFT minting..");
+    println!("ergs box for fee :{}", ergs_box_for_fee.box_id());
+   let unsigned_tx = GachaponProtocol::mint(nft,
+                                            block_height, //current_height
+                                            artist_address, // artist address
+
+                                            tx_fee,
                            ergs_box_for_fee
-    );
+   );
+    println!("minting nft");
+    let tx_id = node.sign_and_submit_transaction(&unsigned_tx).unwrap();
+    println!("transaction submitted");
+    println!("TX: {}", tx_id);
 }
 
 pub fn get_ergs_box_for_fee(user_address: String, tx_fee: u64) -> ErgsBox{
@@ -32,6 +43,7 @@ pub fn get_ergs_box_for_fee(user_address: String, tx_fee: u64) -> ErgsBox{
     // Acquire the Ergo Explorer API endpoint in order to find
     // the our `ergs_box_for_bounty`.
     let ergs_box_url = ergs_box_spec
+        //.explorer_endpoint("https://api-testnet.ergoplatform.com/api")
         .explorer_endpoint("https://api.ergoplatform.com/api")
         .unwrap();
     // Make a get request to the Ergo Explorer API endpoint
@@ -42,5 +54,7 @@ pub fn get_ergs_box_for_fee(user_address: String, tx_fee: u64) -> ErgsBox{
         ErgsBox::process_explorer_response_custom(&get_response, ergs_box_spec).unwrap();
 
     // Return the first `ErgsBox` from the list
+    //println!("erg boxes: {}", list_of_ergs_boxes[0].ergo_box);
     list_of_ergs_boxes[0].clone()
+    
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,73 +1,107 @@
+use ergo_headless_dapp_framework::encoding::build_token;
 pub use ergo_headless_dapp_framework::*;
+use ergo_lib::ergotree_ir::chain::ergo_box::{ErgoBox, ErgoBoxCandidate};
+use ergo_lib::ergotree_ir::chain::token::{Token, TokenAmount, TokenId};
 
-#[derive(Debug, Clone, WrapBox, SpecBox)]
-pub struct NFTBox {
-    ergo_box: ErgoBox,
+/*
+Metadata of an NFT
+*/
+pub struct NFTMetadata {
+    pub name: String,
+    pub description: String,
+    pub asset_type: String,
+    pub url: String,
+    pub bytes: Vec<u8>,
 }
 
-impl SpecifiedBox for NFTBox {
-    fn box_spec() -> BoxSpec {
-        let address = Some("94hWSMqgxHtRNEWoKrJFGVNQEYX34zfX68FNxWr".to_string());
-        BoxSpec::new(address, None, vec![], vec![])
+impl NFTMetadata {
+    pub fn hash(&self) -> String {
+        return "some_hash".to_string();
     }
 }
 
-pub fn upload_img(){
-    println!("uploading image")
+pub struct NFTBox {}
+
+impl NFTBox {
+    /*
+    returns a box that can be used in a transaction to create a new NFT.
+    A ergo NFT is a new Token whose amount is 1 with some metadata in its registers.
+    this is based on the spec done by anon-real's auction house definition:
+    https://github.com/anon-real/ErgoAuctionHouse/blob/master/src/auction/issueArtworkAssm.js#L29
+    */
+    pub fn candidate_box(
+        fee: u64,
+        metadata: NFTMetadata,
+        address: &P2PKAddressString,
+        current_height: BlockHeight,
+        ergs_box_for_fee: &ErgsBox,
+    ) -> Result<ErgoBoxCandidate> {
+        let amount = 1;
+        let decimals = 0;
+
+        let tokens = vec![build_token(&ergs_box_for_fee.box_id(), amount).unwrap()];
+
+        let registers = vec![
+            // R4
+            Constant::from(metadata.name.as_bytes().to_vec()),
+            // R5
+            Constant::from(metadata.description.as_bytes().to_vec()),
+            // R6
+            Constant::from(decimals),
+            // R7
+            Constant::from(metadata.asset_type.as_bytes().to_vec()),
+            // R8
+            Constant::from(metadata.hash().as_bytes().to_vec()),
+            // R9
+            Constant::from(metadata.url.as_bytes().to_vec()),
+        ];
+
+        println!("about to create candidate..");
+        return create_candidate(fee, address, &tokens, &registers, current_height);
+    }
 }
 
 pub struct GachaponProtocol {}
 
-impl GachaponProtocol {
-
-    fn nft_issue_candidate() {
-
-    }
-
-    pub fn claim(){
-        // given an NFTBox it claims it
-    }
-
-    pub fn pull_lever(){
-        // claims a random NFTbox
-    }
-
-    pub fn mint_many() {
-        // mint many nfts in a single transaction
-    }
-
-    pub fn mint(name: String, description: String,
-                royalty: f32, start_gacha_height: u64,
-                artist_fee: u64, current_height: u64,
-                artist_address: String, transaction_fee: u64,
-                ergs_box_for_fee: ErgsBox) -> UnsignedTransaction {
-
-         let tx_inputs = vec![
-            ergs_box_for_fee.as_unsigned_input(),
-        ];
-
-        let nft_candidate = create_candidate(
-            1,
-            &"94hWSMqgxHtRNEWoKrJFGVNQEYX34zfX68FNxWr".to_string(),
-            &vec![],
-            &vec![],
+impl GachaponProtocol { 
+    // returns an unsigned transaction to mint an nft
+    pub fn mint(
+        metadata: NFTMetadata,
+        current_height: u64,
+        artist_address: String,
+        transaction_fee: u64,
+        ergs_box_for_fee: ErgsBox,
+    ) -> UnsignedTransaction {
+        println!("building nft box");
+        let erg_in_box = 100000;
+        let nft_box_candidate = NFTBox::candidate_box(
+            erg_in_box,
+            metadata,
+            &artist_address,
             current_height,
-        ) .unwrap();
+            &ergs_box_for_fee,
+        )
+        .unwrap();
+
+        let tx_inputs = vec![ergs_box_for_fee.as_unsigned_input()];
 
         let total_nano_ergs = ergs_box_for_fee.nano_ergs();
-        let total_change = total_nano_ergs  - transaction_fee;
+        let total_change = total_nano_ergs - erg_in_box - transaction_fee;
+
+        let transaction_fee_candidate =
+            TxFeeBox::output_candidate(transaction_fee, current_height).unwrap();
 
         let change_box_candidate =
-            ChangeBox::output_candidate(&vec![],
-                                        total_change,
-                                        &artist_address,
-                                        current_height)
+            ChangeBox::output_candidate(&vec![], total_change, &artist_address, current_height)
                 .unwrap();
 
         let output_candidates = vec![
-            nft_candidate,
+            nft_box_candidate,
+            transaction_fee_candidate,
             change_box_candidate,
         ];
+
+        println!("returning transaction");
 
         UnsignedTransaction::new(
             tx_inputs.try_into().unwrap(),
@@ -75,6 +109,5 @@ impl GachaponProtocol {
             output_candidates.try_into().unwrap(),
         )
         .unwrap()
-
     }
 }
